6a833f0e309d3f587db6d447640e7bba9b03c13c
imojaek <imojaek@gmail.com>
2024-03-11 18:06:53 +0900

    요청받은 경로의 파일 내용 반환
    HTTP 요청의 시작라인에서 경로를 파싱하고,
    
    그 경로의 파일을 바이트로 변환해서 HTTP 응답을 보냅니다.
    
    여기서는 html 이외의 확장자에 대한 처리가 필요합니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,11 +4,13 @@ import java.io.*;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+import Parser.RequestLineParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String BASE_PATH = "./src/main/resources/static";
 
     private Socket connection;
 
@@ -22,12 +24,14 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
-            // printRequest(in);
+            //readRequestLine(in);
+            // printRequestAll(in);
 
             // TODO 사용자 요청에 대한 처리는 이 위에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
+            String requestPath = RequestLineParser.parsePath(readRequestLine(in));
 
-            byte[] body = getHtml("./src/main/resources/static/index.html").getBytes(); // 여기가 Response Body인가보다.
+            byte[] body = getHtml(BASE_PATH + requestPath).getBytes(); // 여기가 Response Body.
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -35,8 +39,16 @@ public class RequestHandler implements Runnable {
         }
     }
 
+    // request 의 시작 라인만 읽어 콘솔에 출력해 무엇을 요청하는지 확인할 수 있다.
+    private String readRequestLine(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line = br.readLine();
+        logger.debug("Request : {}", line);
+        return line;
+    }
+
     // br의 끝까지 읽으면서 br의 내용을 출력한다.
-    private void printRequest(InputStream in) throws IOException {
+    private void printRequestAll(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
         String line;
         line = br.readLine();
@@ -57,7 +69,7 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
-        return sb.toString(); // 여기가 Response Body인가보다.
+        return sb.toString();
     }
 
 
