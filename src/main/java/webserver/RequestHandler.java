04478e1fd4c3e7e89c8bcbc6c0ba7c187e93f3a1
imojaek <imojaek@gmail.com>
2024-03-11 13:39:50 +0900

    응답 메시지에 index.html 파일 내용 담아보기
    ./src/main/resources/static/index.html 경로의 html 파일을 응답메시지에 넣어 브라우저에서의 출력을 확인했습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,7 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -23,15 +21,45 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            // printRequest(in);
+
+            // TODO 사용자 요청에 대한 처리는 이 위에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
+
+            byte[] body = getHtml("./src/main/resources/static/index.html").getBytes(); // 여기가 Response Body인가보다.
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
+
+    // br의 끝까지 읽으면서 br의 내용을 출력한다.
+    private void printRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line;
+        line = br.readLine();
+        logger.debug("Request : {}", line);
+        while ((line = br.readLine()) != null && (line.length() > 1)) {
+            logger.debug("Header : {}", line);
+        }
+    }
+
+    // 파일의 경로를 매개로 받아 해당 파일의 내용을 반환
+    private String getHtml(String path) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return sb.toString(); // 여기가 Response Body인가보다.
+    }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
