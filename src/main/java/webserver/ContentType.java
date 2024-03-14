3188acc43f7859ed8dd16a76a20a566860ee7893
imojaek <imojaek@gmail.com>
2024-03-14 16:11:55 +0900

    JS, PNG, JPEG 확장자를 ContentType enum에 등록
    JS, PNG, JPEG 확장자를 추가 등록했습니다.

--- src/main/java/webserver/ContentType.java
+++ src/main/java/webserver/ContentType.java
@@ -4,7 +4,10 @@ public enum ContentType {
     HTML("text/html"),
     CSS("text/css"),
     SVG("image/svg+xml"),
-    ICO("image/x-icon");
+    ICO("image/x-icon"),
+    JS("text/javascript"),
+    PNG("image/png"),
+    JPEG("image/jpeg");
     private String contentTypeMsg;
     ContentType(String contentTypeMsg) {
         this.contentTypeMsg = contentTypeMsg;
