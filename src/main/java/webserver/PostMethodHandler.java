b51f2ad32b4fea256efc51fdbcfd8a84ec86523b
imojaek <imojaek@gmail.com>
2024-03-19 15:46:24 +0900

    로그인 시 ID / PW 확인 절차 추가
    회원의 아이디와 비밀번호가 일치하는 지 확인하는 과정을 추가했습니다.
    
    현재, 로그인 성공 시 /main/index.html,
    로그인 실패 시 /index.html 로 리다이렉트 합니다.

--- src/main/java/webserver/PostMethodHandler.java
+++ src/main/java/webserver/PostMethodHandler.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
@@ -15,12 +16,15 @@ public class PostMethodHandler {
     private Map<String, BiConsumer<DataOutputStream, HttpRequest>> actionMap = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(PostMethodHandler.class);
     private final HttpResponse httpResponse = new HttpResponse();
+    private HashMap<String, String> dataMap;
+
 
     public PostMethodHandler() {
         makeActionMap();
     }
 
-    public HttpResponse actionByPath(DataOutputStream dos, HttpRequest request) {
+    public HttpResponse actionByPath(DataOutputStream dos, HttpRequest request) throws UnsupportedEncodingException {
+        dataMap = parseDataString(new String(request.getBody(), "UTF-8"));
         for (String definedPath : actionMap.keySet()) {
             if (definedPath.equals(request.getPath())) {
                 actionMap.get(definedPath).accept(dos, request);
@@ -32,12 +36,27 @@ public class PostMethodHandler {
         return this.httpResponse;
     }
 
-    private void addNewUser(DataOutputStream dos, HttpRequest request) throws IOException {
-        HashMap<String, String> dataMap = parseDataString(new String(request.getBody(), "UTF-8"));
+    private void addNewUser(DataOutputStream dos, HttpRequest request) {
         Database.addUser(new User(dataMap.get("userid"), dataMap.get("password"), dataMap.get("name"), dataMap.get("email")));
         logger.debug("새로운 회원 등록 userID : " + dataMap.get("userid"));
 
         setRedirectReponse(request, "/index.html");
+    }
+
+    private void loginUser(DataOutputStream dos, HttpRequest request) {
+        if (authorizeUser(request)) {
+            setRedirectReponse(request, "/main/index.html");
+            return ;
+        }
+        setRedirectReponse(request, "/index.html");
+    }
+
+    private boolean authorizeUser(HttpRequest request) {
+        User targetUser = Database.findUserById(dataMap.get("login_id"));
+        if (targetUser != null && targetUser.getPassword().equals(dataMap.get("login_password"))) {
+            return true;
+        }
+        return false;
     }
 
     private HashMap<String, String> parseDataString(String dataString) {
@@ -52,11 +71,10 @@ public class PostMethodHandler {
 
     private Map<String, BiConsumer<DataOutputStream, HttpRequest>> makeActionMap() {
         actionMap.put("/user/create", (inputDos, inputRequest) -> {
-            try {
-                addNewUser(inputDos, inputRequest);
-            } catch (IOException e) {
-                logger.error(e.getMessage());
-            }
+            addNewUser(inputDos, inputRequest);
+        });
+        actionMap.put("/user/login", (inputDos, inputRequest) -> {
+            loginUser(inputDos, inputRequest);
         });
         return actionMap;
     }
