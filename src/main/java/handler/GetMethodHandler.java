6f5a83e081872fd2a8b820da6cf832c7a57b9228
imojaek <imojaek@gmail.com>
2024-03-28 14:09:18 +0900

    유저 목록을 확인할 수 있는 기능 추가
    /user/list 로의 GET 요청을 통해서 유저목록을 확인하는 기능을 추가했습니다.
    
    /user/list.html 을 읽어서 {{{TABLE}}} 이라는 문자열을 유저리스트 테이블을 나타내는 HTML 코드로 바꿔 반환하도록 했습니다.

--- src/main/java/handler/GetMethodHandler.java
+++ src/main/java/handler/GetMethodHandler.java
@@ -10,65 +10,33 @@ import http.ContentType;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetMethodHandler implements HttpRequestHandler{
-
-    private static final String BASE_PATH = "./src/main/resources/static";
-    private final HttpResponseManager httpResponseManager = new HttpResponseManager();
+    private final Map<String, Action> actionMap = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(GetMethodHandler.class);
+
+    public GetMethodHandler() {
+        makeActionMap();
+    }
 
     @Override
     public HttpResponse getResponse(HttpRequest request) {
-        return sendFileResponse(request);
+        return actionByPath(request);
     }
 
-    // sendFile이 실행되는 경우는 Method가 GET이고, Target이 특정한 동작을 요구하지 않는 상황인 경우입니다.
-    public HttpResponse sendFileResponse(HttpRequest request){
-        try {
-            httpResponseManager.setResponseLine(request.getHttpVersion(), 200);
-            ContentType contentType = getContentTypeByPath(request.getPath()); // 확장자가 없는 폴더의 경우, index.html을 호출할 것이므로 HTML을 반환할 것입니다.
-            httpResponseManager.addHeader("Content-Type", contentType.getContentTypeMsg() + ";charset=utf-8");
-            byte[] body = readFileByte(modifyRequestPath(request));
-            httpResponseManager.setBody(body);
-
-            return httpResponseManager.getHttpResponse();
-        } catch (IOException e) {
-            logger.error("파일을 읽던 중 오류가 발생했습니다. : " + e.getMessage());
-            httpResponseManager.set404ErrorResponse(request);
-
-            return httpResponseManager.getHttpResponse();
-        }
-    }
-
-    private String modifyRequestPath(HttpRequest request) {
-        String requestPath = request.getPath();
-        if (requestPath.endsWith("/")) {
-            return BASE_PATH + requestPath + "index.html";
-        }
-        if (!requestPath.contains(".")) {
-            return BASE_PATH + requestPath + "/index.html";
-        }
-        return BASE_PATH + requestPath;
-    }
-
-    // 파일의 경로를 매개로 받아 해당 파일의 내용을 반환
-    private byte[] readFileByte(String path) throws IOException {
-        try (FileInputStream fis = new FileInputStream(new File(path))) {
-            byte[] bytes = fis.readAllBytes();
-            return bytes;
-        }
-    }
-    private ContentType getContentTypeByPath(String path) {
-        // 경로에 .이 없어 index.html 을 참조하게 되는 경우.
-        if (!path.contains("."))
-            return ContentType.HTML;
-
-        String ext = path.substring(path.lastIndexOf(".") + 1).toUpperCase();
-        for (ContentType type : ContentType.values()) {
-            if (type.name().equals(ext)) {
-                return ContentType.valueOf(ext);
+    private HttpResponse actionByPath(HttpRequest request) {
+        for (String definedPath : actionMap.keySet()) {
+            if (definedPath.equals(request.getPath())) {
+                return actionMap.get(definedPath).action(request);
             }
         }
-        throw new IllegalArgumentException("Invalid file extension. : " + path); //  지원하지 않는 확장자인 경우.
+        // 지정된 Path로의 요청이 아닌 경우 해당 Path의 정적파일을 반환
+        return new SendFileHandler().action(request);
+    }
+
+    private void makeActionMap() {
+        actionMap.put("/user/list", new UserlistHandler());
     }
 }
---
+++ src/main/java/handler/UserlistHandler.java
@@ -0,0 +1,78 @@
+package handler;
+
+import db.Database;
+import http.HttpRequest;
+import http.HttpResponse;
+import http.HttpResponseManager;
+import http.StatusCode;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sessions.Session;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class UserlistHandler implements Action{
+    HttpResponseManager httpResponseManager = new HttpResponseManager();
+    private static final String BASE_PATH = "./src/main/resources/static";
+    private static final Logger logger = LoggerFactory.getLogger(UserlistHandler.class);
+
+    @Override
+    public HttpResponse action(HttpRequest request) {
+        if (Session.isValidSession(request.getCookies().get().get("sid")))
+            return sendUserList(request);
+        httpResponseManager.setRedirectReponse(request, "/");
+        return httpResponseManager.getHttpResponse();
+    }
+
+    private HttpResponse sendUserList(HttpRequest request) {
+        try {
+            byte[] html = readFileByte(BASE_PATH + "/user/list.html");
+            String htmlString = new String(html, StandardCharsets.UTF_8);
+            String table = makeTable();
+            htmlString = htmlString.replace("{{{TABLE}}}", table);
+            byte[] body = htmlString.getBytes();
+            httpResponseManager.setResponseLine(request.getHttpVersion(), 200);
+            httpResponseManager.addHeader("Content-Type", "text/html");
+            httpResponseManager.setBody(body);
+            return httpResponseManager.getHttpResponse();
+        } catch (IOException e) {
+            logger.error("파일을 읽던 중 오류가 발생했습니다. : " + e.getMessage());
+            httpResponseManager.set404ErrorResponse(request);
+
+            return httpResponseManager.getHttpResponse();
+        }
+    }
+
+    private byte[] readFileByte(String path) throws IOException {
+        try (FileInputStream fis = new FileInputStream(new File(path))) {
+            byte[] bytes = fis.readAllBytes();
+            return bytes;
+        }
+    }
+
+    private String makeTable () {
+        Collection<User> userList = Database.findAll();
+        StringJoiner tableStringJoiner = new StringJoiner("\n</tr>\n<tr>\n", "<table border=\"1\">\n" +
+                "<tr>\n" +
+                "<th>ID</th><th>Name</th><th>Email</th>\n" +
+                "</tr>\n", "\n</tr>\n</table>");
+
+        for (User user : userList) {
+            StringJoiner columnStringJoiner = new StringJoiner("</td><td>", "<td>", "</td>");
+            columnStringJoiner.add(user.getUserId());
+            columnStringJoiner.add(user.getName());
+            columnStringJoiner.add(user.getEmail());
+            tableStringJoiner.add(columnStringJoiner.toString());
+        }
+
+        return tableStringJoiner.toString();
+    }
+}
