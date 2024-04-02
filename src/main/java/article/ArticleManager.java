13af77176dc30dd17c151e8c51f6f3c35aa938cc
imojaek <imojaek@gmail.com>
2024-04-02 13:50:38 +0900

    ArticleManager 추가
    ArticleDB에 접근하고 값을 처리하는 데에 사용할 클래스입니다.

--- src/main/java/article/ArticleManager.java
+++ src/main/java/article/ArticleManager.java
@@ -2,8 +2,26 @@ package article;
 
 import db.ArticleDatabase;
 
+import java.util.List;
+
 public class ArticleManager {
     public void addArticle(String author, String content) {
         ArticleDatabase.addArticle(new Article(author, content));
+    }
+
+    public boolean hasArticle() {
+        if (ArticleDatabase.getArticleCount() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public List<Article> getArticles() {
+        return ArticleDatabase.getArticles();
+    }
+
+    public Article getLatestArticle() {
+        int articleCount = ArticleDatabase.getArticleCount();
+        return getArticles().get(articleCount - 1);
     }
 }
--- src/main/java/db/ArticleDatabase.java
+++ src/main/java/db/ArticleDatabase.java
@@ -6,10 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleDatabase {
-    public static final List<Article> articles = new ArrayList<>();
+    private static final List<Article> articles = new ArrayList<>();
 
     public static void addArticle(Article article) {
         articles.add(article);
     }
 
+    public static List<Article> getArticles() {
+        return articles;
+    }
+
+    public static int getArticleCount() {
+        return articles.size();
+    }
 }
